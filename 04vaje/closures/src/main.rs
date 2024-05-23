4dd82b1b3f529c93549b73095f05cbb9b26586a7
Luka Ponikvar <luka.ponikvar1@gmail.com>
2024-05-23 18:01:29 +0200

    ze dolgo nisem commital

--- 04vaje/closures/src/main.rs
+++ 04vaje/closures/src/main.rs
@@ -1,3 +1,10 @@
+// kako pišeš teste za sprintane stvari;
+// Un iterator?
+// Velike črke????
+
+
+
+use std::fmt::Display;
 // Napišite različne funkcije, ki kot argument sprejmejo zaprtje (closure) in ga pokličejo z različnimi argumenti, ki ustrezajo sledečim ocaml tipom:
 // Tip zaprtja naj bo čim bolj splošen (Fn, FnOnce, FnMut).
 //  (int -> int) -> int -> int
@@ -11,23 +18,37 @@
 // Vzemite zaporedja iz prejšnjih vaj in naredite nov objekt, ki sprejme zaporedje in ga naredi iterabilnega
 
 
+// fn main() {
+//     let k_dva = |x| {x * 2};
+//     println!("{}", apply(k_dva, 10));
+//     // ali
+//     println!("{}", apply(|x| {x * 2}, 10));
+//     // 
+//     let neki = 3;
+//     println!("{}", apply(|x| {x * neki}, 10));
+//     println!("{}", apply_b(|x| {x * neki}, 10));
+// }
+
+// fn apply<F: FnOnce(i32) -> i32>(func: F, a: i32) -> i32 {
+//     func(a)
+// }
+
+// fn apply_b<F: FnOnce(T1) -> T2, T1, T2>(func: F, a: T1) -> T2 {
+//     func(a)
+// }
+
+// --------------------------------------------------------------------------------------------------------------------------------------
+// ITERATORJI
+// --------------------------------------------------------------------------------------------------------------------------------------
+
 fn main() {
-    let k_dva = |x| {x * 2};
-    println!("{}", apply(k_dva, 10));
-    // ali
-    println!("{}", apply(|x| {x * 2}, 10));
-    // 
-    let neki = 3;
-    println!("{}", apply(|x| {x * neki}, 10));
-    println!("{}", apply_b(|x| {x * neki}, 10));
-}
-
-fn apply<F: FnOnce(i32) -> i32>(func: F, a: i32) -> i32 {
-    func(a)
-}
-
-fn apply_b<F: FnOnce(T1) -> T2, T1, T2>(func: F, a: T1) -> T2 {
-    func(a)
+    naredi_iterator_1(vec![17, 4, 19024]);
+    println!("-----");
+    naredi_iterator_2(vec![String::from("17"), String::from("4"), String::from("19024")]);
+    println!("-----");
+    naredi_iterator_7(vec![None, None, None, Some(17), Some(4), Some(19024)]);
+    println!("-----");
+    naredi_iterator_7(vec![None, None, None, Some(String::from("17")), Some(String::from("4")), Some(String::from("anifdkjaodfij"))]);
 }
 
 // Iteratorji
@@ -43,23 +64,63 @@ fn apply_b<F: FnOnce(T1) -> T2, T1, T2>(func: F, a: T1) -> T2 {
 // vektor Option<T> in vrne število Some-ov
 // odfiltrira števila deljena s 3
 
+fn naredi_iterator_1(vec: Vec<i32>) {
+    vec.iter().for_each(|x| println!("{x}"));
+}
+
+fn naredi_iterator_2(vec: Vec<String>) {
+    vec.iter().for_each(|x| println!("{}", x.len()));
+}
+
+fn naredi_iterator_3(vec: Vec<String>) -> usize {
+    let x = match vec.iter().map(|x| {x.len()}).reduce(|x,y| x+y) {
+        Some(i) => i,
+        None => 0,
+    };
+    x
+}
+
+fn naredi_iterator_4(vec: Vec<(i32,i32)>) -> i32 {
+    vec.iter().map(|(x,y)| x*y).filter(|x| *x>0).sum()
+}
+
+fn naredi_iterator_5(vec1: Vec<i32>, vec2: Vec<i32>) -> Vec<i32> {
+    vec1.iter().zip(vec2.iter()).map(|(x, y)| *x + *y).collect()   
+}
+
+fn naredi_iterator_6(vec1: Vec<i32>, vec2: Vec<i32>) -> i32 {
+    vec1.iter().zip(vec2.iter()).map(|(x, y)| *x * *y).filter(|x| *x>0).sum()
+}
+
+fn naredi_iterator_7<T: Display>(vec: Vec<Option<T>>) {
+    vec.iter().for_each(|x| match x {Some(x) => println!("{x}"), _ => ()});
+}
+
+fn naredi_iterator_8<T>(vec: Vec<Option<T>>) -> i32 {
+    vec.iter().map(|x| {match x {Some(_) => 1, None => 0}}).sum()
+}
+
 // Dopolnite spodnjo funkcijo, da vrne niz, kjer so vse prve črke posameznih besed velike
 // ["Just,", " ", "hello", " ", "world", "!"] -> "Just, Hello World", "!"
-pub fn capitalize_words_string(words: &[&str]) -> String {
-    panic!("Not implemented");
-}
+// pub fn capitalize_words_string(words: &[&str]) -> String {
+//     words.iter().map(|x| &format("{}", (**x).capitalize())).reduce(|x ,y| x ^ y)
+// }
 // Napišite funkcijo `fakulteta`, ki izračuna fakulteto števila n. Uporabite iteratorje (torej brez lastne for zanke, rekurzije)
 // Namig: fold, reduce, `..`...
+// fn fakulteta(n: i32) -> i32 {
+
+// }
+
 
 // -------------------------------------------------------------------------------------------------
-/// Dodatno:
+// Dodatno:
 // Koda vzeta iz googlvih rust vaj:
 // Vse se da lepo narediti samo z iteratorji (brez indeksov, brez for zank, brez mutabilnosti)
 /*
-/// Calculate the differences between elements of `values` offset by `offset`,
-/// wrapping around from the end of `values` to the beginning.
-///
-/// Element `n` of the result is `values[(n+offset)%len] - values[n]`.
+// Calculate the differences between elements of `values` offset by `offset`,
+// wrapping around from the end of `values` to the beginning.
+//
+// Element `n` of the result is `values[(n+offset)%len] - values[n]`.
 fn offset_differences<N>(offset: usize, values: Vec<N>) -> Vec<N>
 where
     N: Copy + std::ops::Sub<Output = N>,
@@ -102,3 +163,55 @@ fn test_degenerate_cases() {
 
 */
 
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = main();
+        assert_eq!(result, ());
+    }
+
+    #[test]
+    fn test_naredi_iterator_3() {
+        assert_eq!(naredi_iterator_3(vec![String::from("17"), String::from("4"), String::from("19024")]), 8);
+        assert_eq!(naredi_iterator_3(vec![String::from(""), String::from(""), String::from("")]), 0);
+        assert_eq!(naredi_iterator_3(vec![String::from(""), String::from("Lalalala"), String::from("")]), 8);
+    }
+
+    #[test]
+    fn test_naredi_iterator_4() {
+        assert_eq!(naredi_iterator_4(vec![(17,0), (4,-3), (19024,1)]), 19024);
+        assert_eq!(naredi_iterator_4(vec![(17,0), (4,-3), (19024,-112)]), 0);
+    }
+    #[test]
+    fn test_naredi_iterator_5() {
+        assert_eq!(naredi_iterator_5(vec![17, 4,-3, 19, 024, 1, 0], vec![17, 4, -3, 19, 024, 1, 7]), vec![34, 8, -6, 38, 48, 2, 7]);
+    }
+
+    #[test]
+    fn test_naredi_iterator_6() {
+        assert_eq!(naredi_iterator_6(vec![1, 4,-3, 19, 1, 0], vec![17, 4, -3, 0, 1, 7]), 43);
+    }
+
+    fn test_naredi_iterator_8() {
+        assert_eq!(naredi_iterator_8(vec![Some(17), Some(4), Some(19024), None]), 3);
+        assert_eq!(
+            naredi_iterator_8(
+            vec![
+                Some(String::from("17")),
+                Some(String::from("4")),
+                None, 
+                None, 
+                None, 
+                Some(String::from("anifdkjaodfij")), 
+                Some(String::from("anifdkjaodfij")), 
+                Some(String::from("anifdkjaodfij"))
+                ]
+            ), 
+            5);
+    }
+}
---
+++ poglavje_22/src/main.rs
@@ -0,0 +1,63 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+// Request: [
+//     "GET / HTTP/1.1",
+//     "Host: 127.0.0.1:7878",
+//     "Connection: keep-alive",
+//     "Cache-Control: max-age=0",
+//     "sec-ch-ua: \"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+//     "sec-ch-ua-mobile: ?0",
+//     "sec-ch-ua-platform: \"Windows\"",
+//     "Upgrade-Insecure-Requests: 1",
+//     "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+//     "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+//     "Sec-Fetch-Site: none",
+//     "Sec-Fetch-Mode: navigate",
+//     "Sec-Fetch-User: ?1",
+//     "Sec-Fetch-Dest: document",
+//     "Accept-Encoding: gzip, deflate, br, zstd",
+//     "Accept-Language: sl-SI,sl;q=0.9,en-GB;q=0.8,en;q=0.7",
+// ]
+
+// Method Request-URI HTTP-Version CRLF
+// headers CRLF
+// message-body
+
+
+
+// IP internet protocol
+// ICP transmission control protocol
+// HTTP hypertext transfer protocol
